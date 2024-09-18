debc64265592eecec3b5d09b518f6a3665bbd090
IntanAlfaraAudia <intanalfaradia@gmail.com>
2024-09-18 20:53:08 +0800

    Add files via upload

---
+++ Praktikum APD/Kelas A1'2024 Informatika/pertemuan 3/IntanAlfaraAudia_pertemuan3.py
@@ -0,0 +1,29 @@
+#cuaca = input("Masukkan cuaca hari ini: ")
+#if cuaca == " cerah ": 
+#    print("pergi ke sekolah")
+#elif cuaca == " hujan ": 
+#    print("tetap di rumah")
+#else :
+#    print("invalid")
+
+#nilai = int(input("Masukkan nilai: "))
+#if nilai == 1: 
+#    print (1)
+
+#a = int(input("masukkan nilai 1: "))
+#b = int(input("masukkan nilai 2: "))
+#if a > b :
+#   print("kelinci")
+#elif a < b : 
+#   print ("kura-kura")
+#else : 
+#   print ("kerbau")
+
+#angka = int(input(" masukkan angka "))
+#if angka > 30 :
+#    print ("sapi")
+#    if angka >50:
+#       print ("singa")
+#else: 
+#    print ("invalid")
+    
