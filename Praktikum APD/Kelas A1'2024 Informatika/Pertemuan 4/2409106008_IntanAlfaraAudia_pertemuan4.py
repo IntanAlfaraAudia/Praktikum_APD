37891529c3281481131ed017c2b597c39d04b39d
IntanAlfaraAudia <intanalfaradia@gmail.com>
2024-09-25 09:17:28 +0800

    Add files via upload

---
+++ Praktikum APD/Kelas A1'2024 Informatika/Pertemuan 4/2409106008_IntanAlfaraAudia_pertemuan4.py
@@ -0,0 +1,59 @@
+# ulang = 10 
+# for i in range(ulang): 
+#     print(f"perulangan ke-{str(i)}")
+
+# simpan = [12, "udin petot", 14.5, True, 'A']
+# for i in simpan: 
+#     print(i, end = ' ')
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# jawab =  'ya'
+# hitung = 0
+# while( jawab == 'ya'): 
+#     hitung += 2
+#     jawab = input("ulang lagi tidak: ")
+# print (f"total perulangan : {hitung}")
+
+# hitung = 0
+# while True:
+# hitung += 1
+# ulang = input("ulang lagi tidak? : ")
+# if ulang == "tidak" or ulang =="Tidak":
+# break
+
+# print(f"Total Perulangan: {hitung}")
+
+
+# print("Daftar bilangan ganjil dari 1-10")
+# for i in range(10):
+#     if i % 3 == 0:
+#         continue
+#     print(i)
+
+# bilangan = 0 
+# while True : 
+#         angka = int(input("Maskkan angka: "))
+#         if angka < 0 : 
+#                 break 
+#         bilangan += angka
+# print (f"Total penjumlahan bilangan positif: {bilangan}")
+
+# ulang = 20
+# for i in range(1, 20, 3)
+#     if i % 2 == 0
+#     continue
+#     print(i)
+
+
+# buah = ["apel", 'pisang', 'jeruk', 'melon', 'semangka']
+# buah_baru = []
+
+# for i in buah: 
+#     if "e" in i:
+#         buah_baru.append(i)
+    
+# print(buah_baru)
